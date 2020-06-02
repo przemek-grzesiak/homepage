3ea9337423b226fe5fc94ba1a4d50de763ab7ccd
Przemek Grzesiak <przemek@grzesiak.it>
2020-06-02 21:13:25 +0200

    inner.HTML test

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 firstName = "Przemek";
 age = 36;
 console.log(`Mam na ${firstName} i mam ${age} lat`);
+
+const DOM = document.querySelector('.DOM--js');
+DOM.innerHTML = 'Ta treść nie jest częśćią HTML';
+console.log(DOM);
