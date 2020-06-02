8dbfbf87184db0542e5b5b559aabeb6b65ea10c2
Przemek Grzesiak <przemek@grzesiak.it>
2020-06-02 21:43:01 +0200

    creating js file for js.html

---
+++ js.js
@@ -0,0 +1,4 @@
+
+const DOM = document.querySelector('.DOM--js');
+DOM.innerHTML = 'Ta treść nie jest częśćią HTML';
+console.log(DOM);
--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 firstName = "Przemek";
 age = 36;
 console.log(`Mam na ${firstName} i mam ${age} lat`);
-
-const DOM = document.querySelector('.DOM--js');
-DOM.innerHTML = 'Ta treść nie jest częśćią HTML';
-console.log(DOM);
